b3736c287add7924d7b3cb3937c1a522c40e65bd
Megan Fago <meganlaynefago@gmail.com>
2019-01-20 22:36:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
-# your code goes here
+def begins_with_r(array)
+array.all? do |tool|
+  tool[0] == "r"
+end
+end
+
+ def contain_a(array)
+ array.select do |word|
+   word.include?("a")
+end
+end
+
+def first_wa(array)
+  array.find do |word|
+  word[0..1] == "wa"
+end
+end
+
+def remove_non_strings(array)
+  array.grep(String)
+end
+  
+def count_elements(array)
+  array.count {|name| name > 0}
+end
